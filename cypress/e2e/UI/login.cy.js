b6f47b0c93d78de5c67212ac23a32e6282e6a7b6
Bellatrix <testeapptestes@gmail.com>
2026-04-12 22:55:44 -0300

    Tirando erro

--- cypress/e2e/UI/login.cy.js
+++ cypress/e2e/UI/login.cy.js
@@ -2,7 +2,7 @@ describe('Login SauceDemo', () => {
   it('Login com sucesso', () => {
     cy.login()
 
-    // Forçando erro 👇
-    cy.get('h1').should('contain', 'ERRO TESTE')
+    cy.url().should('include', '/inventory')
   })
 })
+
